3d46f09e29510a38cea80bee092b46c1ec03fefa
Zach Harris <z95harris@gmail.com>
2020-07-10 13:22:11 -0400

    Setup for adding facebook login, updated personal items

---
+++ src/Blogifier.Core/Services/FacebookService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blogifier.Core.Services
+{
+    class FacebookService
+    {
+        //facebook access token
+        //EAAtR1XsZB7ZCYBAHHYDD5rGycmRcs2YpCZAzZCrCPxwcGGV3mJUiZAMLJ6xeBj3Yuh5rh1HoVYb1hMRaWzJxZCnTqKgd5WEhg2AkF8KAOcBgTeeZAhQCwTQS3wcZC68w206iuMOx5ZAQqRQgMGWIVOhdJuQdbDaiLmA4fHzhBmfTzGgZDZD
+    }
+}
